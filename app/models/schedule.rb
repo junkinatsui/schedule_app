77682d629d3a8d6fe36fb71268ddf5623b156094
junkinatsui <dptgagtma0573@gmail.com>
2024-06-03 23:46:32 +0900

    fix/空欄で入力した際のバリデーションの追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,7 +6,10 @@ class Schedule < ApplicationRecord
     validate :start_end_check
 
   def start_end_check
-    errors.add(:end_day, "は開始日より前の日付は登録できません。") unless
-    self.start_day < self.end_day 
+    if start_day.nil? || end_day.nil?
+      errors.add(:base, "開始時間と終了時間を設定してください")
+    elsif start_day >= end_day
+      errors.add(:base, "開始時間は終了時間より前に設定してください")
+    end
   end
 end
